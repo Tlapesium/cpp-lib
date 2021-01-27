55ae4bd0cd806c430a644980e919a047d6f06ac1
Tlapesium <tahuna.sakana@gmail.com>
2021-01-27 15:27:20 +0900

    update segtree

--- data-structure/segment_tree.cpp
+++ data-structure/segment_tree.cpp
@@ -6,52 +6,52 @@
  * @title Segment Tree
  */
 
-template <class Monoid, class MonoidOp>
+template <class Monoid, class Operator>
 struct SegmentTree {
 	std::vector<Monoid> dat;
 
-	const MonoidOp F;
-	const Monoid E;
+	const Operator op;
+	const Monoid IE;
 	const int sz;
 
 	// 単位元で初期化されたセグ木を構築 O(N)
-	SegmentTree(int n, MonoidOp F, Monoid E) : F(F), E(E), sz(n) { dat.assign(sz * 2 + 1, E); }
+	SegmentTree(int n, Operator F, Monoid IE) : op(F), IE(IE), sz(n) { dat.assign(sz * 2 + 1, IE); }
 
 	// vectorを基に構築 O(N)
-	SegmentTree(const std::vector<Monoid>& vec, MonoidOp F, Monoid E) : F(F), E(E), sz(vec.size()) {
-		dat.assign(sz * 2 + 1, E);
+	SegmentTree(const std::vector<Monoid>& vec, Operator F, Monoid IE) : op(F), IE(IE), sz(vec.size()) {
+		dat.assign(sz * 2 + 1, IE);
 		for (int i = 0; i < sz; i++) dat[i + sz] = vec[i];
-		for (int i = sz - 1; i > 0; i--) dat[i] = F(dat[i << 1 | 0], dat[i << 1 | 1]);
+		for (int i = sz - 1; i > 0; i--) dat[i] = op(dat[i << 1 | 0], dat[i << 1 | 1]);
 	}
 
 	int size() { return sz; }
-
 	const Monoid& operator[] (int idx) { return dat[idx + sz]; }
 
-	// fold(l,l+1,...,r-1) を返す
+	// 区間 [l,r) の積を取得
 	Monoid get(int l, int r) {
 		l += sz; r += sz;
-		Monoid lm(E), rm(E);
+		Monoid lm(IE), rm(IE);
 		while (l < r) {
-			if (l & 1)lm = F(lm, dat[l++]);
-			if (r & 1)rm = F(dat[--r], rm);
+			if (l & 1)lm = op(lm, dat[l++]);
+			if (r & 1)rm = op(dat[--r], rm);
 			l >>= 1; r >>= 1;
 		}
-		return F(lm, rm);
+		return op(lm, rm);
 	}
 
+	// 要素 idx を x に更新
 	void set(int idx, Monoid x) {
 		idx += sz; dat[idx] = x;
 		while (idx > 1) {
 			idx >>= 1;
-			dat[idx] = F(dat[idx << 1 | 0], dat[idx << 1 | 1]);
+			dat[idx] = op(dat[idx << 1 | 0], dat[idx << 1 | 1]);
 		}
 	}
 
 	template<class DetermineFunc>
 	int internal_search(Monoid sum, int i, DetermineFunc isOK) {
 		while (i < sz) {
-			Monoid tmp = F(sum, dat[i << 1]);
+			Monoid tmp = op(sum, dat[i << 1]);
 			if (isOK(tmp)) i = i << 1;
 			else sum = tmp, i = (i << 1) | 1;
 		}
@@ -63,11 +63,11 @@ struct SegmentTree {
 	int search_r(DetermineFunc isOK, int l = 0) {
 		l += sz;
 		int r = sz * 2, tmp = 0;
-		Monoid sum = E;
+		Monoid sum = IE;
 		for (int r_ = r; l < r_; r_ >>= 1, l >>= 1, tmp++) {
 			if (l & 1) {
-				if (isOK(F(sum, dat[l])))return internal_search(sum, l, isOK);
-				sum = F(sum, dat[l]);
+				if (isOK(op(sum, dat[l])))return internal_search(sum, l, isOK);
+				sum = op(sum, dat[l]);
 				l++;
 			}
 		}
@@ -75,8 +75,8 @@ struct SegmentTree {
 			int r_ = r >> tmp;
 			if (r_ & 1) {
 				r_--;
-				if (isOK(F(sum, dat[r_])))return internal_search(sum, r_, isOK);
-				sum = F(sum, dat[r_]);
+				if (isOK(op(sum, dat[r_])))return internal_search(sum, r_, isOK);
+				sum = op(sum, dat[r_]);
 			}
 		}
 		return -1;
@@ -87,12 +87,12 @@ struct SegmentTree {
 	}
 };
 
-template<class Monoid, class MonoidOp>
-SegmentTree<Monoid, MonoidOp> makeSegTree(int n, MonoidOp F, Monoid E) {
-	return SegmentTree<Monoid, MonoidOp>(n, F, E);
+template<class Monoid, class Operator>
+SegmentTree<Monoid, Operator> makeSegTree(int n, Operator F, Monoid IE) {
+	return SegmentTree<Monoid, Operator>(n, F, IE);
 }
 
-template<class Monoid, class MonoidOp>
-SegmentTree<Monoid, MonoidOp> makeSegTree(const std::vector<Monoid>& vec, MonoidOp F, Monoid E) {
-	return SegmentTree<Monoid, MonoidOp>(vec, F, E);
+template<class Monoid, class Operator>
+SegmentTree<Monoid, Operator> makeSegTree(const std::vector<Monoid>& vec, Operator F, Monoid IE) {
+	return SegmentTree<Monoid, Operator>(vec, F, IE);
 }
