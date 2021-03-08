875c3d69ae347bfe1a687554dbfe5d5d151a8944
Tlapesium <tahuna.sakana@gmail.com>
2021-03-08 23:41:41 +0900

    update

--- graph/topological_sort.cpp
+++ graph/topological_sort.cpp
@@ -7,12 +7,12 @@ auto topological_sort(const Graph& g) {
 	bool cycle_flag = false;
 	std::vector<int> ret, used(g.size(), 0);
 
-	auto dfs = [&](auto&& dfs, const Graph& g, int pos) -> void {
+	auto dfs = [&](auto&& dfs, int pos) -> void {
 		if (used[pos] == 1) cycle_flag = true;
 		else if (used[pos] == 0) {
 			used[pos] = 1;
-			for (int i = 0; i < g[pos].size(); i++) {
-				dfs(dfs, g, g[pos][i].to);
+			for (auto&& e : g[pos]) {
+				dfs(dfs, e.to);
 			}
 			used[pos] = 2;
 			ret.push_back(pos);
@@ -20,7 +20,7 @@ auto topological_sort(const Graph& g) {
 	};
 
 	for (int i = 0; i < g.size(); i++) {
-		if (used[i] == 0)dfs(dfs, g, i);
+		if (!used[i])dfs(dfs, i);
 		if (cycle_flag)return std::vector<int>();
 	}
 	std::reverse(ret.begin(), ret.end());
