f69633c2a10a8adf81471565363f7d0b092922c6
Tlapesium <tahuna.sakana@gmail.com>
2021-03-12 02:14:29 +0900

    add lca

---
+++ documents/lca.md
@@ -0,0 +1,13 @@
+---
+title: 最小共通祖先
+documentation_of: //graph/lca.cpp
+---
+
+## 説明
+無向木 $g$ の最小共通祖先を求める。
+
+## 操作
+### LowestCommonAncester(g)
+無向木 $g$ をもとに構築する。
+### query(u,v)
+$u$ と $v$ の最小共通祖先を求める。
---
+++ graph/lca.cpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+#include <utility>
+#include "graph/graph.cpp"
+#include "data-structure/sparse_table.cpp"
+
+struct LowestCommonAncester {
+	SparseTable<std::pair<int, int>> ST;
+	std::vector<int> in;
+	LowestCommonAncester(const Graph& g) {
+		std::vector<std::pair<int, int>> tmp;
+		in.resize(g.size());
+		auto dfs = [&](auto&& dfs, int pos, int par, int depth) -> void {
+			if (par == -1) tmp.push_back({ 0,pos });
+			in[pos] = tmp.size();
+			for (auto&& e : g[pos]) {
+				if (e.to == par)continue;
+				tmp.push_back({ depth + 1, e.to });
+				dfs(dfs, e.to, pos, depth + 1);
+				tmp.push_back({ depth,pos });
+			}
+		};
+		dfs(dfs, 0, -1, 0);
+		ST = SparseTable<std::pair<int, int>>(tmp);
+	}
+
+	int query(int u, int v) {
+		return ST.get(std::min(in[u], in[v]) - 1, std::max(in[u], in[v])).second;
+	}
+};
